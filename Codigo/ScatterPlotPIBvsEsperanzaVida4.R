b7d9ae234d384f58298249df97d835a0f34f3d34
IanUgaldeTec <167656913+IanUgaldeTec@users.noreply.github.com>
2025-05-08 17:27:42 -0600

    YA SALE LA GRAFICA

--- Codigo/ScatterPlotPIBvsEsperanzaVida4.R
+++ Codigo/ScatterPlotPIBvsEsperanzaVida4.R
@@ -19,20 +19,21 @@ vida_pib_2022 <- data_long %>%
   filter(!is.na(PIB_per_capita), !is.na(Esperanza_vida))
 
 # Crear el scatterplot interactivo
-plot_ly(
-  data = vida_pib_2022,
-  x = ~PIB_per_capita,
-  y = ~Esperanza_vida,
-  type = 'scatter',
-  mode = 'markers',
-  text = ~paste("País:", País,
-                "<br>PIB per cápita: $", round(PIB_per_capita, 0),
-                "<br>Esperanza de vida:", round(Esperanza_vida, 1), "años"),
-  marker = list(size = 10, color = 'blue', opacity = 0.7)
-) %>%
-  layout(
-    title = "Relación entre PIB per cápita y Esperanza de vida (2022)",
-    xaxis = list(title = "PIB per cápita (US$)", type = "log"),
-    yaxis = list(title = "Esperanza de vida (años)")
-  )
-
+print(
+    plot_ly(
+    data = vida_pib_2022,
+    x = ~PIB_per_capita,
+    y = ~Esperanza_vida,
+    type = 'scatter',
+    mode = 'markers',
+    text = ~paste("País:", País,
+                  "<br>PIB per cápita: $", round(PIB_per_capita, 0),
+                  "<br>Esperanza de vida:", round(Esperanza_vida, 1), "años"),
+    marker = list(size = 10, color = 'blue', opacity = 0.7)
+  ) %>%
+    layout(
+      title = "Relación entre PIB per cápita y Esperanza de vida (2022)",
+      xaxis = list(title = "PIB per cápita (US$)", type = "log"),
+      yaxis = list(title = "Esperanza de vida (años)")
+    )
+)
